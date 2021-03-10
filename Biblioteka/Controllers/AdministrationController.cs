634880a5c665344564a4f3b98d0d98ce122ec176
Ozyrys24 <48652183+Ozyrys24@users.noreply.github.com>
2021-03-10 17:44:28 +0100

    AddRoles
    Add roles to project

--- Biblioteka/Controllers/AdministrationController.cs
+++ Biblioteka/Controllers/AdministrationController.cs
@@ -11,9 +11,13 @@ namespace Biblioteka.Controllers
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager,
+                                        UserManager<IdentityUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
         public IActionResult CreateRole()
         {
@@ -52,10 +56,116 @@ namespace Biblioteka.Controllers
             return View(roles);
         }
 
-        public IActionResult EditRole()
+        public async Task<IActionResult> EditRole(string id)
         {
-            return View();
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            var model = new EditRoleModel
+            {
+                Id = role.Id,
+                RoleName = role.Name,
+                Users = new List<string>()
+            };
+
+            foreach(var user in _userManager.Users)
+            {
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                { 
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
         }
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleModel model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.Id);
+            role.Name = model.RoleName;
+            _ = await _roleManager.UpdateAsync(role);
+
+            return RedirectToAction("ListRoles");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUserInRole(string roleId)
+        {
+            ViewBag.roleId = roleId;
+
+            var role = await _roleManager.FindByIdAsync(roleId);
+
+            if(role == null)
+            {
+                return NotFound();
+            }
+
+            var model = new List<UserRoleModel>();
+
+            foreach(var user in _userManager.Users)
+            {
+                var userRoleModel = new UserRoleModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName
+                };
+
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    userRoleModel.IsSelected = true;
+                }
+                else
+                {
+                    userRoleModel.IsSelected = false;
+                }
+
+                model.Add(userRoleModel);
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUserInRole(List<UserRoleModel> model, string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            foreach (var userModel in model)
+            {
+                var user = await _userManager.FindByIdAsync(userModel.UserId);
+
+                IdentityResult result = null;
+
+                if (userModel.IsSelected && !(await _userManager.IsInRoleAsync(user, role.Name)))
+                {
+                    result = await _userManager.AddToRoleAsync(user, role.Name);
+                }
+                else if(!(userModel.IsSelected) && await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    result = await _userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+                else
+                {
+                    continue;
+                }
+
+                if(!result.Succeeded)
+                {
+                    return RedirectToAction("EditRole", new { Id = roleId });
+                }
+            }
+                return RedirectToAction("EditRole", new { Id = roleId });
+        }
+
         [HttpGet]
         public async Task<IActionResult> DeleteRole(string Id)
         {
---
+++ Biblioteka/Models/EditRoleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteka.Models
+{
+    public class EditRoleModel
+    {
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Role name is Requied")]
+        public string RoleName { get; set; }
+        public List<string> Users { get; set; }
+    }
+}
